f7b11fe99de36948815a302e5c84468ccd655c81
JD Wong <jdmswong@gmail.com>
2016-06-19 11:48:26 -0700

    adds acconts login

--- imports/ui/layouts/App.jsx
+++ imports/ui/layouts/App.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import AccountsUIWrapper from '../components/AccountsUIWrapper.jsx';
 
 // App component - represents the whole app
 export default class App extends Component {
 	render() {
 		return (
-			<h1>HELLO WORLD!</h1>
+			<div>
+
+				<h1>HELLO WORLD!</h1>
+
+				<AccountsUIWrapper />
+
+			</div>
 		);
 	}
 }
