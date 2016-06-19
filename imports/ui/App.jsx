ef7429bed41f2b0ee4bf4efb6dfc146cccef7476
JD Wong <jdmswong@gmail.com>
2016-06-19 11:27:40 -0700

    initial react commit

---
+++ imports/ui/App.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+// App component - represents the whole app
+export default class App extends Component {
+	render() {
+		return (
+			<h1>HELLO WORLD!</h1>
+		);
+	}
+}
