3138cd169dd33b42674d0d9b5ef770fe9655d146
JD Wong <jdmswong@gmail.com>
2016-06-19 11:37:33 -0700

    react router operational

--- client/main.jsx
+++ client/main.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
+import { Router, Route, Link, browserHistory } from 'react-router'
 
 import App from '../imports/ui/layouts/App.jsx';
 
 Meteor.startup(() => {
-	render(<App />, document.getElementById('render-target'));
+	//render(<App />, document.getElementById('render-target'));
+	render((
+		<Router history={browserHistory}>
+			<Route path="/" component={App}>
+
+			</Route>
+		</Router>
+	), document.getElementById('render-target'));
 });
